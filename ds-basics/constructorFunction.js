93579d927eb848e09fe37194a898938b1d060d78
pnegi <prinegi100@gmail.com>
2017-09-21 10:56:56 +0530

    made construction function

---
+++ ds-basics/constructorFunction.js
@@ -0,0 +1,14 @@
+'use strict';
+function User(firstName, lastName, age, gender){
+  this.fisrtName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.gender = gender;
+}
+
+const user1 = new User('priyanka', 'negi', 20, 'female');
+const user2 = new User('aakriti', 'negi', 20, 'female');
+
+
+console.log(user1);
+console.log(user2);
